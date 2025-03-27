1da509501f71a7c6a5781650a5f65c2996e3bf42
Victobhe <victobhe@stud.ntnu.no>
2025-03-27 13:42:22 +0100

    jfijrfirifei

--- src/main/java/prosjekt/Controller.java
+++ src/main/java/prosjekt/Controller.java
@@ -2,8 +2,6 @@ package prosjekt;
 
 
 import java.lang.Thread;
-
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
@@ -55,7 +53,7 @@ public class Controller {
 
     
     @FXML
-    public void handleHit(){
+    private void handleHit(){
         Card kort = deck.deal();
         player.addCard(kort);
         playerScore = player.getSumCard();
@@ -63,25 +61,25 @@ public class Controller {
     }
 
     @FXML
-    public void handleStand(){
+    private void handleStand(){
         Hit.setDisable(true);
         play(player);
         Stand.setDisable(true);
     }
 
     @FXML
-    public void handleSave(){
+    private void handleSave(){
         SaveScore.Save((playerScore + dealerScore), penger);
     }
     
     @FXML
-    public void handleSlider(){
+    private void handleSlider(){
         slider.setMax(penger);
         this.bet =  (int) slider.getValue();  
         operator42.setText(bet + "");
     }
     @FXML
-    public void handleReset(){
+    private void handleReset(){
         this.playerScore = 0;
         this.dealerScore = 0;
         player.returnCards();
@@ -94,21 +92,20 @@ public class Controller {
     }
 
     @FXML
-    public void updateScore(){
+    private void updateScore(){
         if (playerScore >= 22){
             Hit.setDisable(true);
-            Stand.onActionProperty();
         }
         totalSum.setText(String.valueOf(playerScore));
     }
     
     @FXML
-    public void updateDealerScore(){
+    private void updateDealerScore(){
         totalSumField.setText(String.valueOf(dealerScore));
     }
 
     @FXML
-    public boolean didPlayerWin(){
+    private boolean didPlayerWin(){
         if (playerScore > 21){
             return false;
         }
@@ -118,32 +115,29 @@ public class Controller {
         return true;
     }
     @FXML
-    public void play(CardHand hand){
+    private void play(CardHand hand){
         dealer.mustBeat = hand.getSumCard();
         dealer.totalsum = dealer.getSumCard();
-
-        new Thread(() -> {
-            try{
-                while (dealer.standOrHit()){
-                    updateDealerScore();
-                    Card kort = deck.deal();
-                    dealer.addCard(kort);
-                    dealer.totalsum = dealer.getSumCard();
-                    Platform.runLater(() -> updateDealerScore());
-                    Thread.sleep(1000);      
-                    }
-                this.dealerScore = dealer.getSumCard();
-            }catch (InterruptedException e){
-                totalSumField.setText("Error");
-                e.printStackTrace();
+            while (dealer.standOrHit()){
+                Card kort = deck.deal();
+                dealer.addCard(kort);
+                dealer.totalsum = dealer.getSumCard();
+                dealerScore = dealer.getSumCard();
+                updateDealerScore();
+                sleeper();
             }
 
-        }).start();
-        updatePenger();
+            updatePenger();          
     }
-
+    private void sleeper(){
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("legge seg!");
+        }
+    }
     @FXML
-    public void updatePenger(){
+    private void updatePenger(){
         if (didPlayerWin()) {
             this.penger = penger + bet;
             operator4.setText(penger + "kr");

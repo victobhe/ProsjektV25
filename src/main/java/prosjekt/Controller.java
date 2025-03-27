29f6aa943df82a76b721745bbe79ca2ad24d6c56
Victobhe <victobhe@stud.ntnu.no>
2025-03-27 08:54:35 +0100

    kddfjdks

--- src/main/java/prosjekt/Controller.java
+++ src/main/java/prosjekt/Controller.java
@@ -37,8 +37,8 @@ public class Controller {
 
     private int playerScore = 0;
     private int dealerScore = 0;
-    private double penger = 150;
-    private double bett;
+    private double penger = 100;
+    private double bet;
     
     CardDeck deck = new CardDeck();
     CardHand player = new CardHand();
